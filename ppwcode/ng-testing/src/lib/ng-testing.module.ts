b1785b85d0984903169f62914f5293dfe5854264
Glen Staes <glen_staes@peopleware.be>
2021-09-02 08:45:15 +0200

    Remove unnecessary auto-generated content

--- ppwcode/ng-testing/src/lib/ng-testing.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { NgTestingComponent } from './ng-testing.component';
-
-@NgModule({
-    declarations: [NgTestingComponent],
-    imports: [],
-    exports: [NgTestingComponent]
-})
-export class NgTestingModule {}
--- ppwcode/ng-testing/src/public-api.ts
+++ ppwcode/ng-testing/src/public-api.ts
@@ -2,6 +2,3 @@
  * Public API Surface of ng-testing
  */
 
-export * from './lib/ng-testing.service';
-export * from './lib/ng-testing.component';
-export * from './lib/ng-testing.module';
