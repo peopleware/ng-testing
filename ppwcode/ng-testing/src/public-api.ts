167f75c61515b37bd3a9961a727c1e62eaa0bf6a
Glen Staes <glen_staes@peopleware.be>
2021-09-02 09:47:48 +0200

    Add initial implementation

--- ppwcode/ng-testing/src/public-api.ts
+++ ppwcode/ng-testing/src/public-api.ts
@@ -1,3 +1,7 @@
 /*
- * Public API Surface of ng-testing
+ * Public API Surface of @ppwcode/ng-testing
  */
+
+export * from './lib/constants';
+export * from './lib/http/http-call-tester';
+export * from './lib/http/http-client-testing-controller';
