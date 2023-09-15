13339c2a5a943e31cf84cfc809cbbbe18f39047d
Glen Staes <glen_staes@peopleware.be>
2023-09-15 12:48:23 +0200

    Add utility for throwing http error responses

--- ppwcode/ng-testing/src/public-api.ts
+++ ppwcode/ng-testing/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/constants'
 export * from './lib/http/http-call-tester'
 export * from './lib/http/http-client-testing-controller'
+export * from './lib/http/throw-error-response'
